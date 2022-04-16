bc6d632679b8bfd674dec037f6c5d1dd75527ceb
Naresh Challala <100545264+NARESH1603@users.noreply.github.com>
2022-04-16 07:40:59 +0100

    Update Server.py

--- Server.py
+++ Server.py
@@ -13,3 +13,32 @@ DISCONNECT_MESSAGE = "!DISCONNECT"
 # creating socket and binding address and port
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Address fami;y for ipv4 and socket streaming
 server_socket.bind(ADDR)
+def decrypt(encrypted_message, key):
+    outText = []
+    cryptText = []
+    
+    uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    for eachLetter in encrypted_message:    
+        if eachLetter in uppercase:
+            index = uppercase.index(eachLetter)
+            crypting = (index - key) % 26
+            cryptText.append(crypting)
+            newLetter = uppercase[crypting]
+            outText.append(newLetter)
+        elif eachLetter in lowercase:
+            index = lowercase.index(eachLetter)
+            crypting = (index - key) % 26
+            cryptText.append(crypting)
+            newLetter = lowercase[crypting]
+            outText.append(newLetter)
+        elif eachLetter == ' ':
+            outText.append(' ')
+    return outText
+  
+  def handle_client(conn, addr):
+    """ Handle clients connection """
+    print("[NEW CONNECTION] {0} connected".format(addr))
+    connected = True
+    while connected:
